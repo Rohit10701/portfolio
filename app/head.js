48ae5413b7a6eac18aff53e3a20a7aff80283d51
Rohit10701 <rohitkumar10701@gmail.com>
2023-09-28 21:23:50 +0530

    website v2.2

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>Meet rohit</title>
+    <title>Meet Rohit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
